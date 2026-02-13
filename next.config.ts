0145da69d75a9928e2a9efa2e11a4551ae156fab
Sean <sonners.sean@gmail.com>
2026-02-14 00:02:16 +0900

    portone

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   
-  // ✅ 이미지 최적화를 위한 도메인 허용 설정 (필수!)
+  // ✅ 이미지 최적화를 위한 도메인 허용 설정 (필수!//필수 
   images: {
     remotePatterns: [
       {
